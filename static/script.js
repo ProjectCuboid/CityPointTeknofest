61282816e309ce2689996210692570ec65e70051
Project-Cuboid <plazma.ugur@gmail.com>
2025-01-31 09:53:32 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -4,9 +4,12 @@ function getstarted(){
 
 function showLoader() {
     document.getElementById("loader").style.display = "flex";
+    document.body.style.overflow = "hidden";
+    document.body.style.pointerEvents = "none";
 }
 function hideLoader() {
     document.getElementById("loader").style.display = "none";
+    document.body.style.overflow = "auto";
+    document.body.style.pointerEvents = "auto";
 }
-
 setTimeout(hideLoader, 6000);
