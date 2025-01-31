fca422220a05b8fb06dccd2c73111d8499d4ebe6
Project-Cuboid <plazma.ugur@gmail.com>
2025-01-31 09:47:11 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,12 @@
 function getstarted(){
     window.location.href = "/login";
 }
+
+function showLoader() {
+    document.getElementById("loader").style.display = "flex";
+}
+function hideLoader() {
+    document.getElementById("loader").style.display = "none";
+}
+
+setTimeout(hideLoader, 6000);
